c0fc275c0c74812ffd58a67228e689539da234fd
juninhomax <sar.max@hotmail.fr>
2025-11-27 13:03:23 +0100

    code update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1>🎉 EPITECH T-CLO-901 - Zero Downtime Deployment SUCCESS! v1.1 🚀</h1>
+        <h1>Hello World</h1>
         <p>Counter :<p id="value">{{ $value }}</p></p>
         <button id="add">+1</button>
 
